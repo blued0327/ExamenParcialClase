ab0f0feb64ec3003fae136f531fc0bd9c543636a
UMG <UMG@JOC-LAB-1I2.labjoc.local>
2026-04-28 19:08:25 -0600

    Creacion

---
+++ src/dao/PeliculaDao.java
@@ -0,0 +1,16 @@
+
+package dao;
+
+import connection.CreateConnection;
+import java.sql.*;
+import model.PeliculaModel;
+import java.util.List;
+import java.util.ArrayList;
+
+
+public class PeliculaDao {
+    
+    private final CreateConnection connf = new CreateConnection();
+    
+    //obtener todos
+}
---
+++ src/examenparcial2/ExamenParcial2.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package examenparcial2;
+import connection.CreateConnection;
+
+/**
+ *
+ * @author UMG
+ */
+public class ExamenParcial2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        CreateConnection conn = new CreateConnection();
+        conn.getConnection();
+        
+        
+    }
+    
+}
